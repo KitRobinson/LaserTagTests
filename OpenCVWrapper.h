bd4c28f6c4caca2b3b984ee0db739467f4c4f9c4
Kit Robinson <email": null>
2016-09-05 19:09:39 -0500

    additional backup

---
+++ OpenCVWrapper.h
@@ -0,0 +1,19 @@
+//
+//  OpenCVWrapper.h
+//  OpenCVProject
+//
+//  Created by Apprentice on 9/4/16.
+//  Copyright © 2016 Apprentice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface OpenCVWrapper : NSObject
+
++(NSString *) openCVVersionString;
++(UIImage *) makeGrayscale:(UIImage *) image;
++(UIImage *) matchFeatures:(UIImage *) image thatMatch:(UIImage *) scene;
++(UIImage *) matchFeaturesFLANN:(UIImage *) image thatMatch:(UIImage *) scene;
++(UIImage *) complexMatchFeaturesFLANN:(UIImage *) image thatMatch:(UIImage *) scene;
+@end
